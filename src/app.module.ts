cd4f3e4b3a850fd3715af02d2f230a78ef4ec3a6
Douvry <valentin.jabba@gmail.com>
2022-01-05 09:08:18 +0400

    Mongo connection + config with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(`mongodb://${configuration().database.mongo.host}:` +
+      `${configuration().database.mongo.port}/${configuration().database.mongo.database}`
+    ,{ serverSelectionTimeoutMS: 5000 }),
+    /* MongooseModule.forRoot(`mongodb://${configuration().database.mongo.username}:` +
+      `${configuration().database.mongo.password}@${configuration().database.mongo.host}:` +
+      `${configuration().database.mongo.port}/${configuration().database.mongo.database}` + 
+      `?&authSource=admin`, { serverSelectionTimeoutMS: 5000, useFindAndModify: false }), */
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
